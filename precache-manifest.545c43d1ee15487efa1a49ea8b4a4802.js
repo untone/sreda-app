2595beb30fb27e5e8d59f4a803bc540f4ea878dd
Anton Simonov <me@16051976.ru>
2019-05-16 23:35:38 +0300

    Updates

---
+++ precache-manifest.545c43d1ee15487efa1a49ea8b4a4802.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cce074cb5eb246859076773bda554ec2",
+    "url": "/sreda-app/index.html"
+  },
+  {
+    "revision": "20d53c3ca90fbb2a74b7",
+    "url": "/sreda-app/static/css/main.269ce55c.chunk.css"
+  },
+  {
+    "revision": "d5602cc2dae4155a2558",
+    "url": "/sreda-app/static/js/2.4b60735c.chunk.js"
+  },
+  {
+    "revision": "20d53c3ca90fbb2a74b7",
+    "url": "/sreda-app/static/js/main.43be8c10.chunk.js"
+  },
+  {
+    "revision": "c6098fd3360d2794c9e8",
+    "url": "/sreda-app/static/js/runtime~main.d7df3d8e.js"
+  }
+]);
